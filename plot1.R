b1c263cb4532cf500f9f6ee0b17377ae36d20045
Kazbek <nmb.ten@gmail.com>
2014-05-08 16:24:21 +0400

    plot1 done.

---
+++ plot1.R
@@ -0,0 +1,26 @@
+file_name <- "household_power_consumption.txt"
+
+# small file needs to debug and test plot
+# file_name <- "household_power_consumption_small.txt"
+
+# read file
+df <- read.csv(file_name, sep=";", stringsAsFactors=FALSE)
+
+# get only 2007-02-01 and 2007-02-02
+df <- df[df$Date == "1/2/2007" | df$Date == "2/2/2007", ]
+
+# remove question marks
+cleaned <- df$Global_active_power[df$Global_active_power != "?"]
+
+# convert to numbers
+cleaned <- as.numeric(cleaned)
+    
+# open png device
+png(file="plot1.png", width=480, height=480) 
+
+hist(cleaned, col="red",
+    main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+# close png file device
+dev.off()
+
